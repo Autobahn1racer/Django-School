05e8c5392a3511644bedba6f804ecead67f2b670
Mohan S <satbin123@gmail.com>
2019-06-17 13:04:51 +0530

    Support files

--- test1/Employee2/models.py
+++ test1/Employee2/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class emp(models.Model):
+    empid=models.IntegerField()
+    name=models.CharField(max_length=50)
--- turbo/turbo/settings.py
+++ turbo/turbo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employee',
+    'employee2',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'turbo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'emp_data.sqlite3'),
     }
 }
 
