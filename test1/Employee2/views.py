27f5ed73863e146ffe3c48a9fe2977d462e47a07
Mohan S <satbin123@gmail.com>
2019-06-18 10:22:51 +0530

    Login html

--- test1/Employee2/views.py
+++ test1/Employee2/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from .models import emp
 
 def login(request):
-    if request.method==='POST':
+    if request.method=='POST':
         data=request.POST
         ids=data['loginid']
         pswd=data['password']
@@ -14,3 +14,16 @@ def login(request):
             return render(request,'login.html')
     else:
         return render(request,'login.html')
+def register(request):
+    if request.method=='POST':
+        data=request.POST
+        ename=data['name1']
+        empid=data['empid']
+        newemp=emp(name=ename,empid=empid)
+        newemp.save()
+        return redirect('details')
+    else:
+        return render(request,'register.html')
+def details(request):
+    employees=emp.objects.all()
+    return render(request,'details.html',{'employees':employees})
